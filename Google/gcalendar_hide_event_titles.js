05df60097caa7c368506384656fb4444f2210603
pmendeswork <pm.mendes.work@gmail.com>
2024-01-20 23:43:17 +0000

    -added script details in header - improved names, comment, added timeout to wait for calendar to load since i dont know how to wait for it yet

--- Google/gcalendar_hide_event_titles.js
+++ Google/gcalendar_hide_event_titles.js
@@ -1,50 +1,51 @@
 // ==UserScript==
 // @name         Google Calendar - Hide Event Titles
-// @namespace   
+// @namespace    https://github.com/pmendeswork
 // @version      0.1
-// @description  Hide Event Titles from Google Calendar 
+// @description  Hide Event Titles from Google Calendar
 // @author       Pedro Mendes
 // @match        *://calendar.google.com/*
-// @downloadURL 
-// @updateURL 
-// @run-at document-idle
+// @downloadURL  https://github.com/pmendeswork/UserScripts/blob/master/Google/gcalendar_hide_event_titles.js
+// @updateURL    https://github.com/pmendeswork/UserScripts/blob/master/Google/gcalendar_hide_event_titles.js
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
-  
-  
-  
-  function clearEventTitles() {
-      var gridDiv = document.querySelectorAll('div[role="grid"]')[1];
-      var buttons = gridDiv.querySelectorAll('div[role="presentation"] div[role="button"]');
-      buttons.forEach(function (button) {
-          button.textContent = '';
-      });
-  }
-  
-  
-  // Find the li element that contains the "Lixo" span
-  var lixoLi = Array.from(document.querySelectorAll('li')).find(li => li.textContent.includes('Lixo'));
-  
-  if (lixoLi) {
-      // Clone the li element
-      var newLi = lixoLi.cloneNode(true);
-  
-      // Remove the 'jsaction' attribute from the cloned li
-      newLi.removeAttribute('jsaction');
-  
-      // Find the span inside the cloned li and change its text
-      var span = newLi.querySelector('span[jsname="K4r5Ff"]');
-      if (span) {
-          span.textContent = 'Hide Labels';
-      }
-  
-      // Add a click event listener to the new li
-      newLi.addEventListener('click', clearEventTitles);
-  
-      // Insert the new li into the DOM, for example, after the original li
-      lixoLi.parentNode.insertBefore(newLi, lixoLi.nextSibling);
-  }
-  
-  })();
+
+    console.log("Google Calendar - Hide Event Titles script loaded");
+
+    // Function to clear event titles
+    function clearEventTitles() {
+        var gridDiv = document.querySelectorAll('div[role="grid"]')[1];
+        if (gridDiv) {
+            var buttons = gridDiv.querySelectorAll('div[role="presentation"] div[role="button"]');
+            buttons.forEach(function (button) {
+                button.textContent = '';
+            });
+        }
+    }
+
+    // Function to add the 'Hide Event Titles' menu option
+    function addHideEventTitlesOption() {
+        var templateMenuItem = Array.from(document.querySelectorAll('li')).find(li => li.textContent.includes('Lixo'));
+        if (templateMenuItem) {
+            var hideTitlesMenuItem = templateMenuItem.cloneNode(true);
+            hideTitlesMenuItem.removeAttribute('jsaction');
+
+            var span = hideTitlesMenuItem.querySelector('span[jsname="K4r5Ff"]');
+            if (span) {
+                span.textContent = 'Hide Event Titles';
+            }
+
+            hideTitlesMenuItem.addEventListener('click', clearEventTitles);
+            templateMenuItem.parentNode.insertBefore(hideTitlesMenuItem, templateMenuItem.nextSibling);
+            console.log("Hide Event Titles option added");
+        } else {
+            // Retry once more if the menu item is not found
+            setTimeout(addHideEventTitlesOption, 1000);
+        }
+    }
+
+    // Delay initial execution to ensure the page has loaded
+    setTimeout(addHideEventTitlesOption, 2 * 1000);
+})();
